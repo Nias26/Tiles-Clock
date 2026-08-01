6321f8fb0937625e2333636fd843498d9adabef6
Nias26 <genangeliludovico26@gmail.com>
2026-08-01 19:20:38 +0200

    Animate the tiles

--- src/main.c
+++ src/main.c
@@ -3,19 +3,6 @@
 #include <stdio.h>
 #include <time.h>
 
-// TODO: 2026-07-27 00:59 - Nias: change colors/look with shaders
-/* TODO: 2026-07-27 01:00 - Nias: add flip animation
- * For the animation we can display the frames like this:
- * 0 ....
- * 1 ....
- * 2 ....
- * where in `....` we put the falling animation right before the next tile.
- * In here we can create a better struct for the tiles and they all hold
- * their sprites so no offset calculations hell is needed.
- * When the time we get is different from the one we already have, we start the
- * animation.
- * We can animate even the middle colons but I prefrer to leave them still.
- */
 // TODO: 2026-07-31 19:20 - Nias: Add AM - PM toggle
 /* TODO: 2026-07-31 19:21 - Nias: Embed the assets inside the file
  * <https://stackoverflow.com/questions/4158900/embedding-resources-in-executable-using-gcc>
@@ -23,42 +10,61 @@
 
 #define TILE_W 30
 #define TILE_H 65
+#define TILE_FRAMES 6
+#define SPEED 2
 
-void DrawDigit(Texture2D sheet, int digit, int x, int y, float scale) {
-  // Select the tile
-  Rectangle src = {digit * TILE_W, 0, TILE_W, TILE_H};
+typedef struct {
+  int current_digit;
+  int target_digit;
+  int progress;
+  bool is_animating;
+} DigitSlot;
 
-  // Scale the tile
+void DrawDigit(Texture2D sheet, int digit, int frame, int x, int y,
+               float scale) {
+  Rectangle src = {frame * TILE_W, digit * TILE_H, TILE_W, TILE_H};
   Rectangle dst = {x, y, TILE_W * scale, TILE_H * scale};
-  
-  // Draw on the screen
   DrawTexturePro(sheet, src, dst, (Vector2){0, 0}, 0, WHITE);
 }
 
-void DrawColon(Texture2D sheet, int x, int y, float scale) {
-  // Select the colon tile
-  Rectangle src = {2 * TILE_W, TILE_H, TILE_W, TILE_H};
+void DrawAnimatedDigit(Texture2D sheet, DigitSlot *digit, int value, int x,
+                       int start_y, float scale) {
+  float progress_speed = digit->progress / SPEED;
+  digit->target_digit = value;
+  if (digit->current_digit != digit->target_digit) {
+    if (!digit->is_animating)
+      digit->is_animating = true;
 
-  // Scale the tile
-  Rectangle dst = {x, y, TILE_W * scale, TILE_H * scale};
+    DrawDigit(sheet, digit->target_digit, 0, x, start_y, scale);
+    DrawDigit(sheet, digit->current_digit, progress_speed, x, start_y, scale);
 
-  // Draw on the screen
-  DrawTexturePro(sheet, src, dst, (Vector2){0, 0}, 0, WHITE);
+    if (progress_speed < TILE_FRAMES) {
+      digit->progress++;
+    } else {
+      digit->progress = 0;
+      digit->is_animating = false;
+      digit->current_digit = digit->target_digit;
+    }
+  } else {
+    DrawDigit(sheet, value, 0, x, start_y, scale);
+  }
 }
 
 int main(int argc, char **argv) {
-  int screen_width = 800;
-  int screen_height = 600;
+  int screen_width = 523;
+  int screen_height = 165;
 
-  // Enable FullScreeen, VSync and Transparency
-  SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT |
-                 FLAG_WINDOW_TRANSPARENT);
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_WINDOW_TRANSPARENT);
   InitWindow(screen_width, screen_height, "Tiles-Clock");
 
-  // Preapre the spritesheet
   Texture2D sheet = LoadTexture("assets/Tiles.png");
-  // Nearest-Neighbour Upscaling
+  if (!IsTextureValid(sheet)) {
+    fprintf(stderr, "ERROR: Error loading texture");
+    return 1;
+  }
   SetTextureFilter(sheet, TEXTURE_FILTER_POINT);
+  SetTargetFPS(60);
+
   // Shader shader = LoadShader(0, "src/blur.frag");
   // int scale_loc = GetShaderLocation(shader, "pixelScale");
   // float pixel_scale = 2.0f;
@@ -66,72 +72,68 @@ int main(int argc, char **argv) {
 
   Color bg = {39, 39, 39, 255};
 
-  int spacing = 4;
+  int spacing = 0;
   int base_width = (TILE_W * 8) + (spacing * 7);
   int base_height = TILE_H;
 
+  DigitSlot digits[6] = {0};
+
   while (!WindowShouldClose()) {
-    // Get the time
+
     time_t t = time(NULL);
     struct tm *tm = localtime(&t);
 
-    // Get the proper scaling factor
+    int values[6] = {tm->tm_hour / 10, tm->tm_hour % 10, tm->tm_min / 10,
+                     tm->tm_min % 10,  tm->tm_sec / 10,  tm->tm_sec % 10};
+
     float scale_x = (float)GetScreenWidth() / base_width;
     float scale_y = (float)GetScreenHeight() / base_height;
     int scale = (int)floorf(fminf(scale_x, scale_y));
     if (scale < 1)
       scale = 1;
 
-    // Calculate the size of each tile
     int digit_width = TILE_W * scale;
     int digit_height = TILE_H * scale;
-    spacing *= scale;
-    // Calculate the whole clock size
-    int clock_width = (digit_width * 8) + (spacing * 7);
-    int clock_height = digit_height;
+    int scaled_spacing = spacing * scale;
 
-    // Starting position
+    int clock_width = (digit_width * 8) + (scaled_spacing * 7);
     int start_x = (GetScreenWidth() - clock_width) / 2;
-    int start_y = (GetScreenHeight() - clock_height) / 2;
+    int start_y = (GetScreenHeight() - digit_height) / 2;
 
-    int x = start_x; // Just a rename for clarity
+    int x = start_x;
 
     BeginDrawing();
     // BeginShaderMode(shader);
     ClearBackground(bg);
 
     // Hours
-    DrawDigit(sheet, tm->tm_hour / 10, x, start_y, scale);
-    x += digit_width + spacing;
-    DrawDigit(sheet, tm->tm_hour % 10, x, start_y, scale);
-    x += digit_width + spacing;
+    DrawAnimatedDigit(sheet, &digits[0], values[0], x, start_y, scale);
+    x += digit_width + scaled_spacing;
+    DrawAnimatedDigit(sheet, &digits[1], values[1], x, start_y, scale);
+    x += digit_width + scaled_spacing;
 
-    // :
-    DrawColon(sheet, x, start_y, scale);
-    x += digit_width + spacing;
+    DrawDigit(sheet, 12, 0, x, start_y, scale);
+    x += digit_width + scaled_spacing;
 
     // Minutes
-    DrawDigit(sheet, tm->tm_min / 10, x, start_y, scale);
-    x += digit_width + spacing;
-    DrawDigit(sheet, tm->tm_min % 10, x, start_y, scale);
-    x += digit_width + spacing;
+    DrawAnimatedDigit(sheet, &digits[2], values[2], x, start_y, scale);
+    x += digit_width + scaled_spacing;
+    DrawAnimatedDigit(sheet, &digits[3], values[3], x, start_y, scale);
+    x += digit_width + scaled_spacing;
 
-    // :
-    DrawColon(sheet, x, start_y, scale);
-    x += digit_width + spacing;
+    DrawDigit(sheet, 12, 0, x, start_y, scale);
+    x += digit_width + scaled_spacing;
 
     // Seconds
-    DrawDigit(sheet, tm->tm_sec / 10, x, start_y, scale);
-    x += digit_width + spacing;
-    DrawDigit(sheet, tm->tm_sec % 10, x, start_y, scale);
-    x += digit_width + spacing;
+    DrawAnimatedDigit(sheet, &digits[4], values[4], x, start_y, scale);
+    x += digit_width + scaled_spacing;
+    DrawAnimatedDigit(sheet, &digits[5], values[5], x, start_y, scale);
 
     // EndShaderMode();
     EndDrawing();
   }
 
   // UnloadShader(shader);
-  // Cleanup
   UnloadTexture(sheet);
   CloseWindow();
   return 0;
